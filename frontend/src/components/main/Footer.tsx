7e336ebf44f115fcc3029d7cd4285d920a5c822d
Tur-Ochir <turuu@Tur-Ochirs-MacBook-Pro.local>
2025-03-29 14:53:13 +0800

    header footer

--- frontend/src/components/main/Footer.tsx
+++ frontend/src/components/main/Footer.tsx
@@ -1,9 +1,27 @@
 "use client"
+import Link from "next/link";
+import { Button } from "../ui/button"
+import { FaFacebook } from "react-icons/fa";
+
 const Footer = ()=> {
     return(
-        <div>
-            <div className="text-white w-full h-[100px] bg-black">
-                Hello Footer
+        <div className="w-full bg-yellow-600 p-4 shadow-md">
+            <div className="max-w-7xl mx-auto flex flex-col md:flex-row  justify-between h-auto md:h-[80px]">
+                <h1 className="font-bold text-lg md:text-xl lg:text-2xl   md:text-left">
+                    Korean&Japan Express Mongolia
+                </h1> 
+               <div className="font-bold">
+                <h1 className="">Холбоо барих:88393935</h1>
+                <h1>Нүүр</h1>
+                <h1>Барааны цэс</h1>
+                <h1></h1>
+               </div>              
+                 <Link href="https://www.facebook.com/profile.php?id=100086976272987">
+                <Button className="bg-gray-400 hover:bg-amber-300 h-[30px] flex flex-row gap-1 rounded-xl" >
+               <FaFacebook className="w-7 h-7 text-blue-700"/>
+               <h1 className=" font-bold">Korean&Japan Express Mongolia</h1>
+               </Button>
+               </Link>
             </div>
         </div>
     )
--- frontend/src/components/main/Navbar.tsx
+++ frontend/src/components/main/Navbar.tsx
@@ -1,11 +1,23 @@
 "use client"
-const Navbar = ()=> {
-    return(
-        <div>
-            <div className="w-full h-[100px] text-white bg-yellow-600">
-                Hello Navbar
+import { Button } from "@/components/ui/button"
+
+const Navbar = () => {
+    return (
+        <div className="w-full bg-yellow-600 p-4 shadow-md">
+            <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between h-auto md:h-[80px]">
+                <h1 className="font-bold text-lg md:text-xl lg:text-2xl  text-center md:text-left">
+                    Korean&Japan Express Mongolia
+                </h1> 
+               
+               <div className="mt-3 md:mt-0 flex gap-2 overflow-x-auto whitespace-nowrap scrollbar-hide 
+                    w-full md:w-auto justify-start md:justify-center lg:justify-end">
+                    <Button className="border rounded-xl text-sm md:text-base px-3 py-2">Далайн гаралтай бүтээгдэхүүн</Button>
+                    <Button className="border rounded-xl text-sm md:text-base px-3 py-2">Гоо сайхны бүтээгдэхүүн</Button>
+                    <Button className="border rounded-xl text-sm md:text-base px-3 py-2">Эрүүл мэндийн гаралтай бүтээгдэхүүн</Button>
+                    <Button className="border rounded-xl text-sm md:text-base px-3 py-2">Хүнсний бүтээгдэхүүн</Button>
+                </div>         
             </div>
         </div>
     )
 }
-export default Navbar
+export default Navbar;
