9839d06069bc06cbf2bf5f0b8f616cd4fccff899
Benord <Baynaabj749@gmail.com>
2025-04-01 17:38:34 +0800

    backend update all users get

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getMe, loginUser } from "../controllers";
-import { authMiddleware } from "../middleware";
+import { getAllUsers, getMe, loginUser } from "../controllers";
+import { authMiddleware, requireRole } from "../middleware";
 
 const userRouter = Router();
 
 userRouter.post("/login", loginUser);
+userRouter.get("/all", authMiddleware, requireRole, getAllUsers);
 userRouter.get("/me", authMiddleware, getMe);
 
 export default userRouter;
