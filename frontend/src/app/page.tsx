d07d705a687711a7451d875c328c5b3a4f4d4774
Benord <Baynaabj749@gmail.com>
2025-04-03 01:15:05 +0800

    finish

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { Navbar, Footer, Menu, ProductCard } from "@/components/main";
 import { useEffect, useState } from "react";
-import { productType } from "@/types/product";
+import { cartType, productType } from "@/types/product";
 import { api } from "@/lib/axios";
 import { toast } from "sonner";
 import { Shop } from "@/components/main/Shop";
+import { Skeleton } from "@/components/ui/skeleton";
 export default function Home() {
   const [products, setProducts] = useState<productType[]>([]);
   const [categoryId, setCategoryId] = useState<number>();
+  const [cart, setCart] = useState<cartType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     getProducts();
@@ -16,15 +19,20 @@ export default function Home() {
   const getProducts = async () => {
     const token = localStorage.getItem("token");
     try {
+      setLoading(true);
+
       const res = await api.get("/product", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
+
       setProducts(res.data);
     } catch (error) {
       console.error(error);
       toast.error("Барааны мэдээлэл алдаатай байна.");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -33,14 +41,34 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-3 p-1">
       <Navbar />
-      <Menu setCategoryId={setCategoryId} />
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+      <Menu setCategoryId={setCategoryId} loading={loading} />
+      <div
+        style={{ minHeight: "calc(100vh - 112px - 154px)" }}
+        className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4"
+      >
         {(categoryId ? filteredProducts : products).map((product, index) => (
-          <ProductCard key={index} product={product} />
+          <ProductCard key={index} product={product} setCart={setCart} />
         ))}
+        {loading &&
+          Array.from({ length: 8 }).map((_, index) => (
+            <div
+              key={index}
+              className="w-full relative flex flex-col items-center bg-gradient-to-r border-2 border-amber-200 p-4 shadow-xl rounded-lg bg-opacity-90 backdrop-blur-lg"
+            >
+              <div className="w-full h-[200px] flex items-center justify-center overflow-hidden rounded-lg">
+                <Skeleton className="w-full h-full rounded-xl" />
+              </div>
+              <div className="w-full mt-2 text-center">
+                <Skeleton className="h-6 w-3/4 mx-auto" />
+              </div>
+              <div className="w-full mt-2">
+                <Skeleton className="h-5 w-1/2 mx-auto" />
+              </div>
+            </div>
+          ))}
       </div>
       <Footer />
-      <Shop />
+      <Shop cart={cart} setCart={setCart} />
     </div>
   );
 }
--- frontend/src/components/main/Menu.tsx
+++ frontend/src/components/main/Menu.tsx
@@ -4,10 +4,13 @@ import { Button } from "../ui/button";
 import { cartType } from "@/types/product";
 import { api } from "@/lib/axios";
 import { toast } from "sonner";
+import { Skeleton } from "../ui/skeleton";
 export const Menu = ({
   setCategoryId,
+  loading,
 }: {
   setCategoryId: (_categoryId: number) => void;
+  loading: boolean;
 }) => {
   const [categories, setCategories] = useState<cartType[]>([]);
 
@@ -32,15 +35,26 @@ export const Menu = ({
   return (
     <div className="">
       <div className="mt-3 md:mt-0 m-auto flex gap-2 flex-wrap whitespace-nowrap w-full md:w-auto justify-start md:justify-center">
-        {categories.map((category, index) => (
-          <Button
-            key={index}
-            onClick={() => setCategoryId(category.id)}
-            className="border rounded-xl text-sm md:text-base px-3 py-2 bg-gradient-to-r from-yellow-500 to-yellow-700 text-black font-semibold hover:opacity-80 transition "
-          >
-            {category.name}
-          </Button>
-        ))}
+        {loading ? (
+          <div className="mt-3 md:mt-0 m-auto flex gap-2 flex-wrap whitespace-nowrap w-full md:w-auto justify-start md:justify-center">
+            {Array.from({ length: 3 }).map((_, index) => (
+              <Skeleton
+                key={index}
+                className="h-10 w-20 md:w-24 rounded-xl bg-gray-200"
+              />
+            ))}
+          </div>
+        ) : (
+          categories.map((category, index) => (
+            <Button
+              key={index}
+              onClick={() => setCategoryId(category.id)}
+              className="border rounded-xl text-sm md:text-base px-3 py-2 bg-gradient-to-r from-yellow-500 to-yellow-700 text-black font-semibold hover:opacity-80 transition "
+            >
+              {category.name}
+            </Button>
+          ))
+        )}
       </div>
     </div>
   );
